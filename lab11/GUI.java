96ec15433128b620729dc65cc08c6eb981e07e1b
Muhiman-001 <145147758+Muhiman-001@users.noreply.github.com>
2023-11-10 22:28:16 +0500

    Update GUI.java

--- lab11/GUI.java
+++ lab11/GUI.java
@@ -1,6 +1,8 @@
 package lab11;
 
+import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -8,6 +10,7 @@ import java.awt.Label;
 import java.awt.TextField;
 import java.awt.event.ActionListener;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,9 +18,10 @@ import javax.swing.JPanel;
 public class GUI extends JFrame {
 	private JLabel titleLabel;
 
-	private Label lb1;
-	private TextField txt1;
-
+String[] verse = { "ردة حمراء في الحديقة", "تفتحت بألوان مشرقة", "تعطي رائحة جميلة", "تسعد القلب الحزين" };
+	final JComboBox<String> verses = new JComboBox<>(verse);
+	String[] roots= {"ورد","حمر ","حديقة","فتح","ألوان","شرق","عطي","رائحة","جميل","سعد","قلب","حزن"};
+	final JComboBox<String> root = new JComboBox<String>(roots);
 	private Button btn1;
 	public GUI() {
 
@@ -31,37 +35,16 @@ public class GUI extends JFrame {
 		titleLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		JPanel contentPane = new JPanel(new GridLayout(5, 1));
 		contentPane.add(titleLabel);
+		 root.setPreferredSize(new Dimension(100, 30)); 
 
-		lb1 = new Label("Verse : ");
+	        contentPane.add(root, BorderLayout.EAST);
 
-		txt1 = new TextField(16);
-
-		btn1 = new Button("add");
-
-		add(lb1);
-		add(txt1);
-
-		add(btn1);
-
-		setSize(500, 500);
-		setTitle("Login Application");
-		setLayout(new FlowLayout());
-
-	}
-	
-	  public Button get_btn1 ()
-	  {
-		  return btn1;
-	  }
-	  public String getVerse()
-	  {
-		  return txt1.getText();
-	  }
-	  public void setActionListener(ActionListener a)
-	  {
-		  this.btn1.addActionListener(a);
-	  }
-
-	  
-
+verses.setPreferredSize(new Dimension(50, 1));
+		contentPane.add(verses, BorderLayout.WEST);
+		titleLabel.setVisible(true);
+		contentPane.add(titleLabel);
+		verses.setVisible(true);
+		contentPane.add(verses);
+		add(contentPane);
+		 }
 }
