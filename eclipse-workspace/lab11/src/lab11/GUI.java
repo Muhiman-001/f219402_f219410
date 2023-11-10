962b427362a24feb92d6355625d81117c4db3259
Sarah-26 <145147158+Sarah-26@users.noreply.github.com>
2023-11-10 22:38:49 +0500

    Update GUI.java

--- eclipse-workspace/lab11/src/lab11/GUI.java
+++ eclipse-workspace/lab11/src/lab11/GUI.java
@@ -1,7 +1,8 @@
 package lab11;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridLayout;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -10,24 +11,27 @@ import javax.swing.JPanel;
 
 public class GUI extends JFrame {
 	private JLabel titleLabel;
-	String[] roots= {"ورد","حمر ","حديقة","فتح","ألوان","شرق","عطي","رائحة","جميل","سعد","قلب","حزن"};
-	final JComboBox<String> root = new JComboBox<String>(roots);
-public GUI()
-{
-	JFrame frame=new JFrame("poem");
-	frame.setVisible(true);
-	setTitle("Poem Editor");
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setSize(400, 300);
-	setLocationRelativeTo(null);
-	titleLabel = new JLabel("Poems");
-	titleLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
-	JPanel contentPane = new JPanel(new GridLayout(10, 0));
-	titleLabel.setVisible(true);
-	contentPane.add(titleLabel);
-	root.setVisible(true);
-	contentPane.add(root);
-	add(contentPane);
+	String[] roots = { "ورد", "حمر ", "حديقة", "فتح", "ألوان", "شرق", "عطي", "رائحة", "جميل", "سعد", "قلب", "حزن" };
+	
 
-}
+	final JComboBox<String> root = new JComboBox<>(roots);
+
+	public GUI() {
+		setTitle("Poem Editor");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400, 300);
+		setLocationRelativeTo(null);
+
+		titleLabel = new JLabel("Poems");
+		titleLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
+
+		JPanel contentPane = new JPanel(new BorderLayout());
+		contentPane.add(titleLabel, BorderLayout.NORTH);
+		root.setPreferredSize(new Dimension(50, 1));
+		contentPane.add(root, BorderLayout.EAST);
+
+		add(contentPane);
+		setVisible(true);
+	}
+
 }
